20fe208afe641fb2529290b3796951fc869c3d34
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-21 21:26:53 +0800

    adds workflow for authentication

--- app/wrappers/TMDB/api/client.rb
+++ app/wrappers/TMDB/api/client.rb
@@ -2,6 +2,7 @@ module TMDB
     module Api
         class Client
             BASE_URL = "https://api.themoviedb.org/".freeze
+            API_KEY = Rails.application.credentials.dig(:tmdb, :api_key)
             BEARER_TOKEN = Rails.application.credentials.dig(:tmdb, :bearer_token)
 
             def request_token
@@ -12,19 +13,14 @@ module TMDB
                         "Authorization" => "Bearer #{BEARER_TOKEN}",
                         "Content-Type" => "application/json;charset=utf-8"
                 })
-                response = connection.post(
-                    '4/auth/request_token', 
-                    '{"redirect_to": "http://127.0.0.1:3000/user/approved"}', 
-                    {
-                        "Content-Type" => "application/json;charset=utf-8",
-                        "Authorization" => "Bearer #{BEARER_TOKEN}",
-                    } 
-                )
-                body = JSON.parse(response.body)
-                body["request_token"]
+                response = connection.get('3/authentication/token/new', {"api_key" => API_KEY}, {
+                    "Content-Type" => "application/json;charset=utf-8",
+                    "Authorization" => "Bearer #{BEARER_TOKEN}",
+                } )
+                JSON.parse(response.body)["request_token"]
             end
 
-            def access_token(token)
+            def session_id(token)
                 connection = Faraday.new(
                     url: BASE_URL, 
                     params: {},
@@ -32,16 +28,39 @@ module TMDB
                         "Authorization" => "Bearer #{BEARER_TOKEN}",
                         "Content-Type" => "application/json;charset=utf-8"
                 })
-                response = connection.post(
-                    '4/auth/access_token', 
-                    "{'request_token': '#{token}'}", 
-                    {
-                        "Content-Type" => "application/json;charset=utf-8",
+                response = connection.post("3/authentication/session/new?api_key=#{API_KEY}&request_token=#{token}")
+                JSON.parse(response.body)["session_id"]
+            end
+
+            def get_genre
+                connection = Faraday.new(
+                    url: BASE_URL, 
+                    params: {},
+                    headers: {
                         "Authorization" => "Bearer #{BEARER_TOKEN}",
-                    } 
-                )
+                        "Content-Type" => "application/json;charset=utf-8"
+                })
+                response = connection.get("3/genre/movie/list?api_key=#{API_KEY}")
                 JSON.parse(response.body)
             end
+        
+
+            # private
+
+            # def send_request(method, resource, body, parameters)
+            #     connection = Faraday.new(
+            #         url: BASE_URL, 
+            #         params: {},
+            #         headers: {
+            #             "Authorization" => "Bearer #{BEARER_TOKEN}",
+            #             "Content-Type" => "application/json;charset=utf-8"
+            #     })
+            #     response = connection.public_send(method, resource, body, parameters, {
+            #         "Content-Type" => "application/json;charset=utf-8",
+            #         "Authorization" => "Bearer #{BEARER_TOKEN}",
+            #     })
+            #     JSON.parse(response.body)
+            # end
         end
     end
 end
