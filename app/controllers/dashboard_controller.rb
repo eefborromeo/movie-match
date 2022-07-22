7cd16aaad52e026f7c9426a66483069b7dafe79c
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-22 12:37:28 +0800

    creates search page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,4 +9,13 @@ class DashboardController < ApplicationController
             current_user.save
         end
     end
+
+    def search
+        client = TMDB::Api::Client.new
+
+        @search = params[:movie]
+        result_with_spaces = @search.split(" ").join("%20")
+        id = client.search_movie_id(result_with_spaces)["results"][0]["id"]
+        @similar_movies = client.get_similar_movies(id)["results"]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :dashboard, only: [:index]
+  get '/dashboard' => 'dashboard#index'
+  get '/search' => 'dashboard#search'
 
   root to: "dashboard#index"
 end
