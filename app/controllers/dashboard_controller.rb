2f29f56fc0bc1ca0d155c10b157aec2071529a31
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-22 10:02:14 +0800

    refactors to send_request

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
     def index
         client = TMDB::Api::Client.new
-        @token = client.request_token
+        @token = client.get_token["request_token"]
         
         if current_user.session_id == nil && params[:request_token]
             session = client.session_id(params[:request_token])
--- app/wrappers/TMDB/api/client.rb
+++ app/wrappers/TMDB/api/client.rb
@@ -1,66 +1,36 @@
 module TMDB
     module Api
         class Client
-            BASE_URL = "https://api.themoviedb.org/".freeze
+            BASE_URL = "https://api.themoviedb.org/3/".freeze
             API_KEY = Rails.application.credentials.dig(:tmdb, :api_key)
             BEARER_TOKEN = Rails.application.credentials.dig(:tmdb, :bearer_token)
 
-            def request_token
-                connection = Faraday.new(
-                    url: BASE_URL, 
-                    params: {},
-                    headers: {
-                        "Authorization" => "Bearer #{BEARER_TOKEN}",
-                        "Content-Type" => "application/json;charset=utf-8"
-                })
-                response = connection.get('3/authentication/token/new', {"api_key" => API_KEY}, {
-                    "Content-Type" => "application/json;charset=utf-8",
-                    "Authorization" => "Bearer #{BEARER_TOKEN}",
-                } )
-                JSON.parse(response.body)["request_token"]
+            def get_token
+                send_request(:get, "authentication/token/new?api_key=#{API_KEY}")
             end
 
-            def session_id(token)
-                connection = Faraday.new(
-                    url: BASE_URL, 
-                    params: {},
-                    headers: {
-                        "Authorization" => "Bearer #{BEARER_TOKEN}",
-                        "Content-Type" => "application/json;charset=utf-8"
-                })
-                response = connection.post("3/authentication/session/new?api_key=#{API_KEY}&request_token=#{token}")
-                JSON.parse(response.body)["session_id"]
+            def get_session_id(token)
+                send_request(:post, "authentication/session/new?api_key=#{API_KEY}&request_token=#{token}")
             end
 
-            def get_genre
-                connection = Faraday.new(
-                    url: BASE_URL, 
-                    params: {},
-                    headers: {
-                        "Authorization" => "Bearer #{BEARER_TOKEN}",
-                        "Content-Type" => "application/json;charset=utf-8"
-                })
-                response = connection.get("3/genre/movie/list?api_key=#{API_KEY}")
-                JSON.parse(response.body)
+            def get_genres
+                send_request(:get,"/genre/movie/list?api_key=#{API_KEY}" )
             end
         
 
-            # private
+            private
 
-            # def send_request(method, resource, body, parameters)
-            #     connection = Faraday.new(
-            #         url: BASE_URL, 
-            #         params: {},
-            #         headers: {
-            #             "Authorization" => "Bearer #{BEARER_TOKEN}",
-            #             "Content-Type" => "application/json;charset=utf-8"
-            #     })
-            #     response = connection.public_send(method, resource, body, parameters, {
-            #         "Content-Type" => "application/json;charset=utf-8",
-            #         "Authorization" => "Bearer #{BEARER_TOKEN}",
-            #     })
-            #     JSON.parse(response.body)
-            # end
+            def send_request(method, resource)
+                connection = Faraday.new(
+                    url: BASE_URL, 
+                    params: {},
+                    headers: {
+                        "Authorization" => "Bearer #{BEARER_TOKEN}",
+                        "Content-Type" => "application/json;charset=utf-8"
+                })
+                response = connection.public_send(method, resource)
+                JSON.parse(response.body)
+            end
         end
     end
 end
